f7abf33d2230e004a97a3e59ac85c426bf18d9d6
n0way <n0way@localhost.localdomain>
2020-06-26 13:14:16 +0200

    class

--- src/netflix_activity/netflix_selenium.py
+++ src/netflix_activity/netflix_selenium.py
@@ -48,111 +48,137 @@ def handle_NoSuchElementException(element):
         sys.exit(1)
 
 
-def login(driver):
-    """Identification de l'utilisateur sur netflix.
+class Netflix:
+    download_dir = os.path.dirname(os.path.abspath(__file__))
 
-    Requires globals EMAIL and PASSWORD
-    """
-    email_id = "id_userLoginId"
-    password_id = "id_password"
+    def __init__(self, headless):
+        self.driver = self.__get_driver(headless)
 
-    with handle_NoSuchElementException(email_id):
-        driver.find_element_by_id(email_id).send_keys(EMAIL)
+    def __enter__(self):
+        return self
 
-    with handle_NoSuchElementException(password_id):
-        entry = driver.find_element_by_id(password_id)
+    def __exit__(self, type, value, traceback):
+        self.driver.quit()
 
-    entry.send_keys(PASSWORD)
-    entry.submit()  # wait for the next page to load
+    @staticmethod
+    def __get_driver(headless):
+        options = Options()
+        options.headless = headless
+        # To prevent download dialog
+        options.set_preference("browser.download.folderList", 2)  # custom location
+        options.set_preference("browser.download.dir", Netflix.download_dir)
+        options.set_preference(
+            "browser.helperApps.neverAsk.saveToDisk",
+            "text/plain,text/x-csv,text/csv,application/csv,text/comma-separated-values,text/x-comma-separated-values,text/tab-separated-values",
+        )
 
+        driver = webdriver.Firefox(options=options)
+        driver.get(
+            "https://www.netflix.com/SwitchProfile?tkn=7J6GIXWIFFA45FGWEV7JRBD7MY"
+        )  # nicolas profile token
+        # L'URL https://www.netflix.com/fr/viewingactivity renvoie directement sur l'historique du premier profil créé
 
-def view_activity(driver):
-    """Déplacement sur la page de visualisation de l'activité de l'utilisateur"""
-    # On change de page
-    with wait_for_page_load(driver):
-        pass
-    driver.get("https://www.netflix.com/fr/viewingactivity")
+        return driver
 
+    def login(self):
+        """Identification de l'utilisateur sur netflix.
 
-def download_seen(driver, download_dir):
-    """Téléchargement de l'historique d'activité au format csv
+        Requires globals EMAIL and PASSWORD
+        """
+        email_id = "id_userLoginId"
+        password_id = "id_password"
 
-    Requires a previous call to view_activity()
-    """
+        with handle_NoSuchElementException(email_id):
+            self.driver.find_element_by_id(email_id).send_keys(EMAIL)
 
-    file_name = "NetflixViewingHistory.csv"
+        with handle_NoSuchElementException(password_id):
+            entry = self.driver.find_element_by_id(password_id)
 
-    download_id = "viewing-activity-footer-download"
-    with handle_NoSuchElementException(download_id):
-        driver.find_element_by_class_name(download_id).click()
+        entry.send_keys(PASSWORD)
+        entry.submit()  # wait for the next page to load
 
-    # On attend la fin du téléchargement pour fermer le driver
-    # Si le fichier est déjà présent, il n'a pas le temps d'être téléchargé
-    while not os.path.exists(download_dir + "/" + file_name):
-        print(f"Fichier téléchargé: {download_dir}/{file_name}")
+    def view_activity(self):
+        """Déplacement sur la page de visualisation de l'activité de l'utilisateur"""
+        # On change de page
+        with wait_for_page_load(self.driver):
+            pass
+        self.driver.get("https://www.netflix.com/fr/viewingactivity")
+
+    def download_seen(self):
+        """Téléchargement de l'historique d'activité au format csv
+
+        Requires a previous call to view_activity()
+        """
+
+        file_name = "NetflixViewingHistory.csv"
+
+        download_id = "viewing-activity-footer-download"
+        with handle_NoSuchElementException(download_id):
+            self.driver.find_element_by_class_name(download_id).click()
+
+        # On attend la fin du téléchargement pour fermer le driver
+        # Si le fichier est déjà présent, il n'a pas le temps d'être téléchargé
+        while not os.path.exists(Netflix.download_dir + "/" + file_name):
+            print(f"Fichier téléchargé: {Netflix.download_dir}/{file_name}")
+            time.sleep(1)
+
+        #  driver.quit()
+
+    def get_rated(self):
+        """Récupération des titres évalués au format csv titre/date
+
+        Requires a previous call to view_activity()
+        """
+
+        # Vérification qu'on est sur la page des titres évalués
+        if "viewing-activity-footer-download" in self.driver.page_source:
+            self.__page_toggle()
+        # On ne change pas d'url donc on ne peut pas utiliser wait_for_page_load()
         time.sleep(1)
+        return self.__get_titles()
 
-    #  driver.quit()
+    def get_seen(self):
+        """Récupération des titres vus au format csv titre/date
 
+        Requires a previous call to view_activity()
+        """
 
-def page_toggle(driver):
-    """Changement de page bidirectionnel entre les titres vus/évalués"""
+        # Vérification qu'on est sur la page des titres vus
+        if "viewing-activity-footer-download" not in self.driver.page_source:
+            self.__page_toggle()
+        # On ne change pas d'url donc on ne peut pas utiliser wait_for_page_load()
+        time.sleep(1)
+        return self.__get_titles()
 
-    page_toggle_class_name = "pageToggle"
-    page_toggle = driver.find_element_by_class_name(page_toggle_class_name)
-    page_toggle.find_element_by_tag_name("a").click()
+    def __page_toggle(self):
+        """Changement de page bidirectionnel entre les titres vus/évalués"""
 
+        page_toggle_class_name = "pageToggle"
+        page_toggle = self.driver.find_element_by_class_name(page_toggle_class_name)
+        page_toggle.find_element_by_tag_name("a").click()
 
-def get_rated(driver):
-    """Récupération des titres évalués au format csv titre/date
+    def __get_titles(self):
+        """Récupération des titres au format csv titre/date"""
 
-    Requires a previous call to view_activity()
-    """
+        titles_dict = defaultdict(list)
 
-    # Vérification qu'on est sur la page des titres évalués
-    if "viewing-activity-footer-download" in driver.page_source:
-        page_toggle(driver)
-    # On ne change pas d'url donc on ne peut pas utiliser wait_for_page_load()
-    time.sleep(1)
-    return get_titles(driver)
+        while True:
+            try:
+                content = self.driver.find_element_by_css_selector(
+                    "button[class='btn btn-blue btn-small']"
+                )
+                content.click()
+            except NoSuchElementException:
+                break
 
+        li_class_name = "retableRow"
+        titles = self.driver.find_elements_by_class_name(li_class_name)
+        for title in titles:
+            # Le premier tag div contient la date de visionnage
+            titles_dict["dates"].append(title.find_element_by_tag_name("div").text)
+            titles_dict["names"].append(title.find_element_by_tag_name("a").text)
 
-def get_seen(driver):
-    """Récupération des titres vus au format csv titre/date
-
-    Requires a previous call to view_activity()
-    """
-
-    # Vérification qu'on est sur la page des titres vus
-    if "viewing-activity-footer-download" not in driver.page_source:
-        page_toggle(driver)
-    # On ne change pas d'url donc on ne peut pas utiliser wait_for_page_load()
-    time.sleep(1)
-    return get_titles(driver)
-
-
-def get_titles(driver):
-    """Récupération des titres au format csv titre/date"""
-
-    titles_dict = defaultdict(list)
-
-    while True:
-        try:
-            content = driver.find_element_by_css_selector(
-                "button[class='btn btn-blue btn-small']"
-            )
-            content.click()
-        except NoSuchElementException:
-            break
-
-    li_class_name = "retableRow"
-    titles = driver.find_elements_by_class_name(li_class_name)
-    for title in titles:
-        # Le premier tag div contient la date de visionnage
-        titles_dict["dates"].append(title.find_element_by_tag_name("div").text)
-        titles_dict["names"].append(title.find_element_by_tag_name("a").text)
-
-    return titles_dict
+        return titles_dict
 
 
 @click.command()
@@ -168,30 +194,11 @@ def get_titles(driver):
 def main(headless):
     """Netflix Activity CLI"""
 
-    download_dir = os.path.dirname(os.path.abspath(__file__))
-
-    options = Options()
-    options.headless = headless
-    # To prevent download dialog
-    options.set_preference("browser.download.folderList", 2)  # custom location
-    options.set_preference("browser.download.dir", download_dir)
-    options.set_preference(
-        "browser.helperApps.neverAsk.saveToDisk",
-        "text/plain,text/x-csv,text/csv,application/csv,text/comma-separated-values,text/x-comma-separated-values,text/tab-separated-values",
-    )
-
-    driver = webdriver.Firefox(options=options)
-    driver.get(
-        "https://www.netflix.com/SwitchProfile?tkn=7J6GIXWIFFA45FGWEV7JRBD7MY"
-    )  # nicolas profile token
-    # L'URL https://www.netflix.com/fr/viewingactivity renvoie directement sur l'historique du premier profil créé
-
-    login(driver)
-    view_activity(driver)
-    rated_titles_dict = get_rated(driver)
-    pprint(rated_titles_dict)
-    seen_titles_dict = get_seen(driver)
-    pprint(seen_titles_dict)
-    # download_seen(driver, download_dir)
-
-    driver.quit()
+    with Netflix(headless) as netflix:
+        netflix.login()
+        netflix.view_activity()
+        rated_titles_dict = netflix.get_rated()
+        pprint(rated_titles_dict)
+        seen_titles_dict = netflix.get_seen()
+        pprint(seen_titles_dict)
+        # netflix.download_seen(download_dir)
