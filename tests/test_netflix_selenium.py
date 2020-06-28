340f47e0133d0996fa9d34a3dae831e978d48f96
n0way <n0way@localhost.localdomain>
2020-06-28 09:39:21 +0200

    functionnal tests

--- tests/test_netflix_selenium.py
+++ tests/test_netflix_selenium.py
@@ -16,8 +16,9 @@ def runner():
 
 
 @pytest.yield_fixture
-def netflix_headless():
+def netflix_headless(tmp_path):
     headless = True
+    netflix_selenium.Netflix.download_dir = str(tmp_path)
     with netflix_selenium.Netflix(headless) as netflix:
         yield netflix
 
@@ -60,24 +61,26 @@ def test_switch_profiles(netflix_headless):
     assert new_profile == PROFILES[0]
 
 
-def test_download_seen(netflix_headless, tmp_path):
-    netflix_headless.download_dir = tmp_path
+def test_download_seen(netflix_headless):
+    """Téléchargement des titres vus"""
 
     netflix_headless.download_seen()
     assert os.path.exists(
-        netflix_headless.download_dir + "/" + netflix_headless.download_file_name
+        netflix_selenium.Netflix.download_dir
+        + "/"
+        + netflix_selenium.Netflix.download_file_name
     )
 
 
-def test_get_json(netflix_headless, tmp_path):
-    netflix_headless.download_dir = tmp_path
+def test_get_json(netflix_headless):
+    """Téléchargement des titres vus/notés au format json"""
 
     # On peut ne pas noter des films/séries...
     rated_titles_dict = netflix_headless.get_rated()
     netflix_headless.save_to_json(rated_titles_dict, "rated_titles.json")
-    assert os.path.exists(netflix_headless.download_dir + "/rated_titles.json")
+    assert os.path.exists(netflix_selenium.Netflix.download_dir + "/rated_titles.json")
     # ...mais on considère que l'utilisateur a visionné au moins un(e) film/série
     seen_titles_dict = netflix_headless.get_seen()
     assert seen_titles_dict
     netflix_headless.save_to_json(seen_titles_dict, "seen_titles.json")
-    assert os.path.exists(netflix_headless.download_dir + "/seen_titles.json")
+    assert os.path.exists(netflix_selenium.Netflix.download_dir + "/seen_titles.json")
